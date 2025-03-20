d7b87fe1a273aefb9ad82e5dc2ae025fd3522804
Alex Beauchemin <abeauche@gmail.com>
2025-03-20 16:49:40 -0700

    Alex expanded script.

--- scripts/vegetation_ordination_QHI.R
+++ scripts/vegetation_ordination_QHI.R
@@ -5,17 +5,19 @@
 # Date Created: 2025-03-20
 # Last Modified: 2025-03-20
 # Description: This script creates an ordination for vegetation plots based on vegetation composition data for 10 POLCAM sites on Qikiqtaruk-Herschel Island measured in the summer of 2024 by A. Beauchemin and E. Bowman.
-# Dependencies: List any required packages or external files.
+# Dependencies: ARU_veg_survey_compiled.csv.
 # ====================================================
 
 # Load required packages
 library(tidyverse)  # Example
+library(vegan)
+
 
 # Set working directory (if needed)
-# setwd("path/to/directory")
+setwd("/Users/alexandrebeauchemin/TundraBUZZ_github")
 
 # Load data
-# data <- read.csv("data/input_file.csv")
+vegetation_raw <- read.csv("/data_raw/QHI_vegetation_2024.csv", stringsAsFactors = TRUE)
 
 # Define functions (if applicable)
 
