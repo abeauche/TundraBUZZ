b907a28b80dd62f1c63aaf6e084acb589a59fadd
Alex Beauchemin <abeauche@gmail.com>
2025-03-27 17:23:39 -0700

    Alex wrote script to wrangle and plot ARUQ4.

--- scripts/flight_buzzes_QHI.R
+++ scripts/flight_buzzes_QHI.R
@@ -3,7 +3,7 @@
 # Project: TundraBUZZ 2024-25
 # Author: Alex Beauchemin
 # Date Created: 2025-03-21
-# Last Modified: 2025-03-25
+# Last Modified: 2025-03-27
 # Description: This script TBD.
 # Dependencies: TBD, location_mapping_TundraBUZZ.csv
 # ====================================================
@@ -294,6 +294,42 @@ ggplot(summary_pred_duration_ARUQ456_2024, aes(x = time_of_day, y = total_durati
   #facet_wrap(~location_id)
 
 
+#### Work with ARUQ4 data ----
+# Define threshold and filter data
+threshold <- 8  
+ARUQ4_2024_above_threshold <- ARUQ4_2024_pred_mapped %>%
+  filter(BUZZ > threshold) %>%
+  mutate(duration_above_threshold = 0.15)  # Each segment is 0.3s, so each overlap segment is 0.15s
+
+# Save csv
+write.csv(ARUQ4_2024_above_threshold, "/Volumes/TundraBUZZ/outputs/recognizer_outputs/clean/ARUQ4_2024_pred_above_threshold.csv", row.names = FALSE)
+
+# Summarize total duration above threshold per datetime
+summary_pred_duration_ARUQ4_2024 <- ARUQ4_2024_above_threshold %>%
+  group_by(datetime, location_id, microclimate) %>%
+  summarize(total_duration_above_threshold = sum(duration_above_threshold), .groups = "drop")
+
+# Convert datetime to UTC-7
+summary_pred_duration_ARUQ4_2024 <- summary_pred_duration_ARUQ4_2024 %>%
+  mutate(time_of_day = hms::as_hms(format(datetime, "%H:%M:%S")))
+
+# Save csv
+write.csv(summary_pred_duration_ARUQ4_2024, "/Volumes/TundraBUZZ/outputs/recognizer_outputs/clean/summary_ARUQ4_2024_pred.csv", row.names = FALSE)
+
+
+# Plot flight buzzes over time
+ggplot(summary_pred_duration_ARUQ4_2024, aes(x = datetime, y = total_duration_above_threshold)) +
+  geom_point() +  
+  labs(title = "Flight Buzzes Over Time (Threshold = 8)",
+       x = "Datetime", 
+       y = "Total Predicted Flight Buzz Duration (s)") +
+  #ylim(0, 100) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(~location_id)
+
+
+
 #### Microclimate data ----
 # Read in microclimate data
 aru_temp_daily_micro <- read.csv("/Users/alexandrebeauchemin/Desktop/Team_Shrub_2024/team_shrub_beauchemin_honours/aru_temp_daily_micro.csv")
@@ -314,6 +350,13 @@ aruq0_data_plot <- aru_temp_daily_micro %>%
 aruq56_data_plot <- aru_temp_daily_micro %>%
   filter(aru_name %in% c("ARUQ5", "ARUQ6")) %>%
   mutate(location_id = recode(aru_name, "ARUQ5" = "WARM1", "ARUQ6" = "COOL1")) %>%
+  mutate(datetime = as.POSIXct(paste(2024, month, day, sep="-"), 
+                               format="%Y-%m-%d", tz="UTC"))
+
+# Filter for ARUQ4
+aruq4_data_plot <- aru_temp_daily_micro %>%
+  filter(aru_name == "ARUQ4") %>%
+  mutate(location_id = recode(aru_name, "ARUQ4" = "COOL4")) %>%
   mutate(datetime = as.POSIXct(paste(2024, month, day, sep="-"), 
                                format="%Y-%m-%d", tz="UTC"))
 
@@ -426,6 +469,108 @@ ggplot() +
 
 
 
+#### Plotting ARUQ4 ----
+# Aggregate total duration above threshold per day
+summary_pred_duration_agg_ARUQ4_2024 <- summary_pred_duration_ARUQ4_2024 %>%
+  mutate(date = as.Date(datetime)) %>%
+  group_by(date, location_id) %>%
+  summarise(total_duration_day = sum(total_duration_above_threshold, na.rm = TRUE), .groups = "drop")
+
+# Merge with daily microclimate data
+merged_data_ARUQ4 <- summary_pred_duration_agg_ARUQ4_2024 %>%
+  filter(location_id == "COOL4") %>%
+  left_join(aruq4_data_plot, by = c("date" = "datetime", "location_id")) %>%
+  filter(!is.na(month))  # Remove NAs in month column
+
+hist(log(merged_data_ARUQ4$mean_value))
+hist(log(merged_data_ARUQ4$total_duration_day))
+
+
+# Fit separate models for each location_id
+models_ARUQ4 <- merged_data_ARUQ4 %>%
+  group_by(location_id) %>%
+  do(model = lm(log(total_duration_day) ~ log(mean_value), data = .))
+
+# Generate predictions for each location_id
+predictions_ARUQ4 <- merged_data_ARUQ4 %>%
+  group_by(location_id) %>%
+  do({
+    model <- lm(log(total_duration_day) ~ log(mean_value), data = .)
+    pred <- predict(model, newdata = ., se.fit = TRUE)
+    tibble(
+      mean_value = .$mean_value,
+      total_duration_day = .$total_duration_day,
+      predicted = exp(pred$fit),  # Convert back from log scale
+      lower_bound = exp(pred$fit - 1.96 * pred$se.fit),
+      upper_bound = exp(pred$fit + 1.96 * pred$se.fit)
+    )
+  })
+
+# Plot total flight buzz duration vs. mean temperature
+ggplot(predictions_ARUQ4, aes(x = mean_value, y = total_duration_day)) +
+  geom_point() +
+  geom_line(aes(y = predicted), color = "skyblue4", size = 1) +  # Regression line
+  geom_ribbon(aes(ymin = lower_bound, ymax = upper_bound), alpha = 0.1) +  # Confidence interval
+  
+  labs(title = "Total Flight Buzz Duration vs. Mean Temperature",
+       x = "Daily Mean Temperature (°C)",
+       y = "Total Flight Buzz Duration (s)") +
+  geom_vline(xintercept = 6, color = "orange", linetype = "dashed", size = 1, alpha = 0.7) +
+  geom_vline(xintercept = 12.6, color = "orange4", linetype = "dashed", size = 1, alpha = 0.7) +
+  annotate("text", x = 6, y = max(predictions_ARUQ4$total_duration_day, na.rm = TRUE) * 0.8, 
+           label = "B. frigidus (Q)", color = "orange", angle = 90, hjust = 0.5, vjust = -1, fontface = "italic") +
+  annotate("text", x = 12.6, y = max(predictions_ARUQ4$total_duration_day, na.rm = TRUE) * 0.8, 
+           label = "B. frigidus (W)", color = "orange4", angle = 90, hjust = 0.5, vjust = -1, fontface = "italic") +
+  theme_classic() +
+  ylim(0,200) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(~location_id)
+
+
+
+class(aruq4_data_plot$datetime)
+aruq4_data_plot$date <- as.POSIXct(aruq4_data_plot$datetime)
+class(summary_pred_duration_agg_ARUQ4_2024$date)
+
+
+
+# Convert date columns to Date or POSIXct
+summary_pred_duration_agg_ARUQ4_2024$date <- as.Date(summary_pred_duration_agg_ARUQ4_2024$date)
+aruq4_data_plot$date <- as.Date(aruq4_data_plot$date)
+
+ggplot() +
+  # Plot the total duration above the threshold over time
+  geom_point(data = summary_pred_duration_agg_ARUQ4_2024, aes(x = date, y = total_duration_day/24), color = "skyblue", alpha = 0.7) +
+  
+  # Temperature curve
+  geom_smooth(data = aruq4_data_plot, aes(x = date, y = mean_value), color = "orange3", method = "loess", alpha = 0.7, se = FALSE) +
+  geom_smooth(data = summary_pred_duration_agg_ARUQ4_2024, aes(x = date, y = total_duration_day/24), color = "skyblue4", method = "loess", se = TRUE) +
+  geom_line(data = aruq4_data_plot, aes(x = date, y = mean_value), color = "orange", alpha = 0.7) +
+  geom_point(data = aruq4_data_plot, aes(x = date, y = mean_value), color = "orange", alpha = 0.7) +
+  
+  # Customizing the plot
+  labs(
+    title = "Flight Buzzes and Temperature Over Time - ARUQ4",
+    x = "Datetime", 
+    y = "Mean Flight Buzz Duration per Hour (seconds)",
+    subtitle = "Blue: Flight Buzzes | Orange: Temperature"
+  ) +
+  
+  # Secondary y-axis for temperature
+  scale_y_continuous(
+    name = "Total Duration Above Threshold (seconds)",
+    limits = c(0, 30),
+    sec.axis = sec_axis(~ ., name = "Temperature (°C)", labels = scales::label_number())
+  ) +
+  
+  # Ensure the x-axis is properly formatted and limited
+  scale_x_date(limits = c(as.Date("2024-06-20"), max(summary_pred_duration_agg_ARUQ4_2024$date, na.rm = TRUE))) +
+  
+  # Clean theme and formatting
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position = "bottom")
+
 
 
 
@@ -454,6 +599,9 @@ flower_summary <- polcam_mapped %>%
   group_by(date, location_id) %>%
   summarise(total_flower_count = sum(flower_count), .groups = "drop")
 
+
+
+#### ARUQ56 ----
 
 # Merge the flower count data with the other datasets
 merged_data <- merge(summary_pred_duration_agg_ARUQ456_2024, flower_summary, by = c("date", "location_id"))
@@ -560,6 +708,118 @@ ggplot(merged_data_clean, aes(x = total_flower_count, y = total_duration_day)) +
   ) +
   theme_classic() +
   facet_wrap(~location_id)
+
+
+
+
+
+
+#### ARUQ4 ----
+# Merge the flower count data with the other datasets
+merged_data_aruq4 <- merge(summary_pred_duration_agg_ARUQ4_2024, flower_summary, by = c("date", "location_id"))
+merged_data_aruq4 <- merge(merged_data_aruq4, aruq4_data_plot, by = c("date", "location_id"))
+
+
+# Filter out rows where total_flower_count is zero
+merged_data_filtered_aruq4 <- merged_data_aruq4 %>%
+  filter(total_flower_count > 0)
+
+# Plot
+ggplot() +
+  # Plot the total duration above the threshold over time
+  geom_point(data = merged_data_aruq4, aes(x = date, y = total_duration_day/24), color = "skyblue", alpha = 0.7) +
+  
+  # Plot the temperature curve
+  geom_smooth(data = merged_data_aruq4, aes(x = date, y = mean_value), color = "orange3", method = "loess", alpha = 0.7, se = FALSE) +
+  geom_smooth(data = merged_data_aruq4, aes(x = date, y = total_duration_day/24), color = "skyblue4", method = "loess", se = FALSE) +
+  geom_line(data = merged_data_aruq4, aes(x = date, y = mean_value), color = "orange", alpha = 0.7) +
+  geom_point(data = merged_data_aruq4, aes(x = date, y = mean_value), color = "orange", alpha = 0.7) +
+  
+  # Add the total flower count as a new series
+  geom_smooth(data = merged_data_filtered_aruq4, aes(x = date, y = total_flower_count), color = "darkolivegreen", size = 1.2, se = FALSE) +
+  geom_point(data = merged_data_filtered_aruq4, aes(x = date, y = total_flower_count), color = "darkolivegreen", size = 2, alpha = 0.7, se = FALSE) +
+  
+  # Customizing the plot
+  labs(
+    title = "Flight Buzzes, Temperature, and Flower Count Over Time - ARUQ4",
+    x = "Datetime", 
+    y = "Mean Flight Buzz Duration per Hour (seconds)",
+    subtitle = "Blue: Flight Buzzes | Orange: Temperature | Green: Flower Count"
+  ) +
+  
+  # Secondary y-axis for temperature
+  scale_y_continuous(
+    name = "Total Duration Above Threshold (seconds)",
+    limits = c(0, 25),
+    sec.axis = sec_axis(~ ., name = "Temperature (°C)", labels = scales::label_number())
+  ) +
+  
+  # Ensure the x-axis is properly formatted and limited
+  scale_x_date(limits = c(as.Date("2024-06-20"), max(merged_data_aruq4$date, na.rm = TRUE))) +
+  
+  # Clean theme and formatting
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position = "bottom")
+
+
+
+
+# Filter out rows with NA, NaN, or Inf values
+merged_data_clean_aruq4 <- merged_data_aruq4 %>%
+  filter(!is.na(total_duration_day), !is.na(total_flower_count)) %>%
+  filter(!is.nan(total_duration_day), !is.nan(total_flower_count)) %>%
+  filter(total_duration_day != Inf, total_flower_count != Inf)
+
+
+hist(log(merged_data_clean_aruq4$total_flower_count))
+
+# Fit separate models for each location_id
+models_ARUQ4_flowering <- merged_data_clean_aruq4 %>%
+  group_by(location_id) %>%
+  do(model = lm(log(total_duration_day) ~ total_flower_count, data = .))
+
+# Generate predictions for each location_id
+predictions_ARUQ4_flowering <- merged_data_clean_aruq4 %>%
+  group_by(location_id) %>%
+  do({
+    model <- lm(log(total_duration_day) ~ total_flower_count, data = .)
+    pred <- predict(model, newdata = ., se.fit = TRUE)
+    tibble(
+      total_flower_count = .$total_flower_count,  # Ensure this column is included
+      total_duration_day = .$total_duration_day,
+      predicted = exp(pred$fit),  # Convert back from log scale
+      lower_bound = exp(pred$fit - 1.96 * pred$se.fit),
+      upper_bound = exp(pred$fit + 1.96 * pred$se.fit)
+    )
+  })
+
+
+# Plotting
+ggplot(merged_data_clean_aruq4, aes(x = total_flower_count, y = total_duration_day)) +
+  geom_point(color = "skyblue") +  # Scatter plot with blue points
+  geom_line(data = predictions_ARUQ4_flowering, aes(x = total_flower_count, y = predicted), color = "skyblue4", size = 1) +
+  geom_ribbon(data = predictions_ARUQ4_flowering, aes(x = total_flower_count, ymin = lower_bound, ymax = upper_bound), alpha = 0.1) +
+  # Add model coefficients and p-values as text annotations
+  # geom_text(data = predictions_ARUQ56_flowering %>%
+  #            group_by(location_id) %>%
+  #            slice(1), 
+  #          aes(x = 1, y = 1600, label = paste("Intercept =", round(coef(models_ARUQ56_flowering)[1], 2), "\np-value =", round(summary(models_ARUQ56_flowering)$coefficients[1,4], 4)),
+  #              color = "black", size = 5)) +  # Intercept annotation with p-value
+  #geom_text(data = predictions_ARUQ56_flowering %>%
+  #            group_by(location_id) %>%
+  #            slice(1),
+  #          aes(x = 1, y = 1400, label = paste("Slope =", round(coef(models_ARUQ56_flowering)[2], 2), "\np-value =", round(summary(models_ARUQ56_flowering)$coefficients[2,4], 4)),
+  #              color = "black", size = 5)) +  # Slope annotation with p-value
+  labs(
+    title = "Total Flight Buzz Duration vs Flowering Count - ARUQ4",
+    x = "Daily Flowering Count",
+    y = "Total Flight Buzz Duration (seconds)"
+  ) +
+  theme_classic() 
+
+
+
 
 
 
