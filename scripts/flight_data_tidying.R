52221fe5df59adeda8338955805527c4d404e532
Alex Beauchemin <abeauche@gmail.com>
2025-03-28 11:29:14 -0700

    Alex created flight_data_tidying.R script.

---
+++ scripts/flight_data_tidying.R
@@ -0,0 +1,12 @@
+# ====================================================
+# Script Name: flight_data_tidying.R
+# Project: TundraBUZZ 2024-25
+# Author: Alex Beauchemin
+# Date Created: 2025-03-28
+# Last Modified: 2025-03-28
+# Description: This script TBD.
+# Dependencies: TBD, location_mapping_TundraBUZZ.csv
+# ====================================================
+
+#### SETUP ----
+# Load required libraries
