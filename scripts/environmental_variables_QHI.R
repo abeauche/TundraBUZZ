b6d6a7cfdd70ebd0e4eca137674bc56a133bc389
Alex Beauchemin <abeauche@gmail.com>
2025-04-07 22:12:24 -0700

    Alex wrote script to combine environmental variables under a large dataframe.

--- scripts/environmental_variables_QHI.R
+++ scripts/environmental_variables_QHI.R
@@ -12,15 +12,98 @@
 # Load required libraries
 library(tidyverse)
 library(lubridate)
-library(suncalc)
 library(hms)
-library(lme4)
-library(lmerTest) 
-library(mgcv)
-library(visreg)
+library(janitor)
 
 # Set working directory
 setwd("/Users/alexandrebeauchemin/TundraBUZZ_github")
 
 # Set seed for repeatability
 set.seed(123)
+
+#### Load data ----
+cloud_cover <- read_csv("/Volumes/TundraBUZZ/data/raw/copernicus_browser_qhi_2024.csv")
+met_station_pauline_cove <- read_csv("/Volumes/TundraBUZZ/data/raw/MetStation_data_QHI_2024/MetStation_PaulineCove_29Jul2024/met_station_pauline_cove_22010631.csv", skip = 1)
+eccc_june <- read_csv("/Volumes/TundraBUZZ/data/raw/ECCC_climate_hourly_herschel/en_climate_hourly_YT_2100636_06-2024_P1H.csv")
+eccc_july <- read_csv("/Volumes/TundraBUZZ/data/raw/ECCC_climate_hourly_herschel/en_climate_hourly_YT_2100636_07-2024_P1H.csv")
+eccc_aug <- read_csv("/Volumes/TundraBUZZ/data/raw/ECCC_climate_hourly_herschel/en_climate_hourly_YT_2100636_08-2024_P1H.csv")
+sun_data <- read_csv("/Users/alexandrebeauchemin/TundraBUZZ_github/data/raw/QHI_sun_data_2024.csv")
+sunrise_daily <- read_csv("/Users/alexandrebeauchemin/TundraBUZZ_github/data/raw/QHI_sunrise_daily.csv")
+
+
+#### Merge and tidy ECCC dataset ----
+# Bind datasets
+eccc_all <- bind_rows(eccc_june, eccc_july, eccc_aug) %>%
+  filter(`Date/Time (LST)` >= as.POSIXct("2024-06-21 00:00:00"),
+         `Date/Time (LST)` <= as.POSIXct("2024-08-12 23:59:59"))
+
+# Select relevant columns (non NA)
+eccc_summer_2024_hourly <- eccc_all %>%
+  select(`Date/Time (LST)`, Year, Month, Day, `Time (LST)`, `Wind Dir (10s deg)`, `Wind Spd (km/h)`, `Stn Press (kPa)`)
+
+# Clean names, mutate wind dir into degrees
+eccc_summer_2024_hourly <- eccc_summer_2024_hourly %>%
+  clean_names() %>%
+  mutate(wind_dir_deg = wind_dir_10s_deg*10) %>%
+  select(- wind_dir_10s_deg)
+
+# Rename columns
+eccc_summer_2024_hourly <- eccc_summer_2024_hourly %>%
+  rename(
+    datetime = date_time_lst,
+    time_hour = time_lst,
+  )
+
+# Aggregate into daily dataset
+eccc_summer_2024_daily <- eccc_summer_2024_hourly %>%
+  mutate(date = as_date(datetime)) %>%  # Create date-only column
+  group_by(date) %>%
+  summarise(
+    mean_wind_speed = mean(wind_spd_km_h, na.rm = TRUE),
+    max_wind_speed = max(wind_spd_km_h, na.rm = TRUE),
+    mean_stn_press_k_pa = mean(stn_press_k_pa, na.rm = TRUE),
+    predominant_wind_dir = names(sort(table(wind_dir_deg), decreasing = TRUE)[1]),
+    .groups = "drop"
+  )
+
+
+#### Select relative humidity from met_station_pauline_cove ----
+# Select RH, mutate datetime as POSIXct
+met_station_pauline_cove <- met_station_pauline_cove %>%
+  select(`Date Time, GMT-06:00`, `RH, % (LGR S/N: 22010631, SEN S/N: 21741589, LBL: air_rh_1m)`) %>%
+  rename(
+    datetime = `Date Time, GMT-06:00`,
+    air_rh_1m = `RH, % (LGR S/N: 22010631, SEN S/N: 21741589, LBL: air_rh_1m)`,
+  ) %>%
+  mutate(
+    datetime = mdy_hms(datetime)  # This assumes the format is MM/DD/YYYY HH:MM:SS
+  )
+
+# Summarize hourly
+met_station_pauline_cove_hourly <- met_station_pauline_cove %>%
+  mutate(
+    datetime_hour = floor_date(datetime, "hour")  # Truncate datetime to the hour
+  ) %>%
+  group_by(datetime_hour) %>%
+  summarise(
+    avg_air_rh_1m = mean(air_rh_1m, na.rm = TRUE),  # Average relative humidity per hour
+    .groups = "drop"
+  )
+
+# Summarize daily
+met_station_pauline_cove_daily <- met_station_pauline_cove %>%
+  mutate(
+    date = as_date(datetime)  # Extract the date from datetime
+  ) %>%
+  group_by(date) %>%
+  summarise(
+    avg_air_rh_1m = mean(air_rh_1m, na.rm = TRUE),  # Average relative humidity per day
+    .groups = "drop"
+  )
+
+#### Correct timezone for sun_data ---- IN PROGRESS
+# Remove " UTC" from the datetime strings and then parse as POSIXct
+sun_data$datetime <- ymd_hms(gsub(" UTC", "", sun_data$datetime))
+
+# Convert the datetime column to the "America/Whitehorse" timezone
+sun_data$datetime <- with_tz(sun_data$datetime, tzone = "America/Whitehorse")
