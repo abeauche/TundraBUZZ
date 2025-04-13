eacc93b6c01cb96ef979d8bd093b1d43ba3fdd6b
Alex Beauchemin <abeauche@gmail.com>
2025-04-12 22:35:36 -0700

    Alex wrote script to collect all recording dates and export figure.

---
+++ scripts/aru_dates_tidy_TundraBUZZ.R
@@ -0,0 +1,109 @@
+# ====================================================
+# Script Name: aru_dates_tidy_TundraBUZZ.R
+# Project: TundraBUZZ 2024-25
+# Author: Alex Beauchemin
+# Date Created: 2025-04-12
+# Last Modified: 2025-04-12
+# Description: This script TBD.
+# Dependencies: summary_flightbuzzes_ARUQ_2024.csv, daily_summary_flightbuzzes_ARUQ_2024.csv, environmental_variables_hourly.csv, environmental_variables_daily.csv, QHI_location_temperature_hourly.csv, QHI_location_temperature_daily.csv, mean_summer_temp_TundraBUZZ.csv, location_mapping_TundraBUZZ.csv, R packages: tidyverse, lubridate, suncalc, hms, lme4, lmerTest, mgcv, visreg, patchwork, cowplot, viridis.
+# ====================================================
+
+#### SETUP ----
+# Load required libraries
+library(tidyverse)
+library(lubridate)
+library(hms)
+library(lme4)
+library(lmerTest) 
+library(mgcv)
+library(visreg)
+library(patchwork)
+library(cowplot)
+library(viridis)
+library(brms)
+library(tidybayes)
+
+# Set working directory
+setwd("/Users/alexandrebeauchemin/TundraBUZZ_github")
+
+# Set seed for repeatability
+set.seed(123)
+
+# Set latitude and longitude for Qikiqtaruk - Herschel Island
+lat <- 69.57
+lon <- -138.91
+
+
+#### Using tidy datasets ----
+# Load data
+flight_buzz_daily <- read_csv("/Volumes/TundraBUZZ/data/clean/flight_buzz_daily.csv")
+flight_buzz_hourly <- read_csv("/Volumes/TundraBUZZ/data/clean/flight_buzz_hourly.csv")
+summary_flightbuzzes_ARUQ_2024 <- read_csv("/Volumes/TundraBUZZ/outputs/recognizer_outputs/clean/summary_flightbuzzes_ARUQ_2024.csv")
+mean_temp <- read_csv("/Volumes/TundraBUZZ/data/clean/mean_summer_temp_TundraBUZZ.csv")
+
+# Make sure datetime is in proper format
+summary_flightbuzzes_ARUQ_2024 <- summary_flightbuzzes_ARUQ_2024 %>%
+  mutate(datetime = str_trim(datetime),
+         datetime = str_replace_all(datetime, "/", "-"),  # replace slashes with dashes
+         datetime = if_else(str_detect(datetime, "^\\d{4}-\\d{2}-\\d{2}$"),
+                            paste0(datetime, " 00:00:00"),
+                            datetime),
+         datetime = ymd_hms(datetime, tz = "UTC"))
+
+summary_flightbuzzes_ARUQ_2024 <- summary_flightbuzzes_ARUQ_2024 %>%
+  mutate(datetime = with_tz(datetime, tzone = "America/Whitehorse"))
+
+
+# Define the expected time step (30 minutes in this case)
+time_step <- as.difftime(30, units = "mins")
+
+# Get min and max datetime per location_id
+recording_windows <- summary_flightbuzzes_ARUQ_2024 %>%
+  group_by(location_id) %>%
+  summarise(
+    start = min(datetime),
+    end = max(datetime),
+    .groups = "drop"
+  )
+
+# Generate full sequence of expected times per location
+expected_times <- recording_windows %>%
+  rowwise() %>%
+  mutate(datetime_seq = list(seq(from = start, to = end, by = time_step))) %>%
+  unnest(datetime_seq) %>%
+  select(location_id, datetime = datetime_seq)
+
+
+
+# Generate tibble with problem dates per location
+bad_dates <- tibble(
+  location_id = c("COOL2", "MOD1", "WARM2"),  # Example location IDs
+  error_date = as.POSIXct(c("2024-08-02 06:10:00", "2024-07-26 13:53:00", "2024-08-01 16:28:00"), tz = "America/Whitehorse"),
+  re_start = as.POSIXct(c("2024-08-04 13:30:00", "2024-08-05 13:00:00", "2024-08-06 16:00:00"), tz = "America/Whitehorse")
+)
+
+# Merge bad dates with the expected_times dataset
+recording_windows_clean <- expected_times %>%
+  left_join(bad_dates, by = "location_id") %>%
+  # Apply filtering condition only to sites with error_date and re_start (i.e., bad dates)
+  filter(is.na(error_date) | !(datetime >= error_date & datetime < re_start)) %>%
+  select(-error_date, -re_start)  # Optionally drop the extra columns
+
+
+# Order locations per mean_summer_temp
+ordered_locations <- mean_temp %>%
+  pull(location_id)
+
+# Visualize active periods as tiles with space between tiles
+active_aru_dates_plot <- ggplot(recording_windows_clean, aes(x = datetime, y = factor(location_id, levels = rev(ordered_locations)))) +
+  geom_tile(color = "forestgreen", width = 0.8, height = 0.8) +  # Adjust width and height for space
+  theme_classic() +
+  labs(x = "2024 Growing Season", y = "Site") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text.y = element_text(size = 10))  # Adjust y-axis text size for better readability
+
+# Save the plot as a PDF
+ggsave("./outputs/figures/active_aru_dates_plot.pdf", 
+       plot = active_aru_dates_plot,   # Save the most recent plot
+       width = 10, height = 6)  # Customize the size as needed
+
